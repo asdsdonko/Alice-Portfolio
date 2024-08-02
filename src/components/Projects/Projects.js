448ab6f1736e3defd30d7dadaca910e41cdc8ce2
Tomas dos Santos <156028270+TomSbados@users.noreply.github.com>
2024-08-02 10:33:06 -0700

    fixing projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -29,7 +29,7 @@ function Projects() {
                   isBlog={false}
                   title="Web Scraping Financial Data"
                   description="Used Python to automate the retrieval of S&P 500 financial data and performed machine learning analysis to find the best performing stocks. "
-                  ghLink="https://github.com/tomsbados"
+                  ghLink="https://github.com/tomsbados/SP500-Machine-Learning"
               />
             </Col>
 
@@ -38,6 +38,26 @@ function Projects() {
                   isBlog={false}
                   title="Language Model"
                   description="Used PyTorch to create a language model trained on my math homework as well as Shakespeare passages."
+                  ghLink="https://github.com/tomsbados/Language-Model-Practice"
+
+              />
+            </Col>
+
+            <Col md={4} className="project-card">
+              <ProjectCard
+                  isBlog={false}
+                  title="Income Prediction Project"
+                  description="This project aims to predict whether an individual's income exceeds $50K per year based on various demographic and employment-related features. "
+                  ghLink="https://github.com/tomsbados/Income-Prediction-Project"
+
+              />
+            </Col>
+
+            <Col md={4} className="project-card">
+              <ProjectCard
+                  isBlog={false}
+                  title="AMES Housing Dataset Kaggle"
+                  description="This project aims to gain in depth skills in advanced regression and feature engineering techniques. I set myself the goal of scoring top 10% on Kaggle."
                   ghLink="https://github.com/tomsbados"
 
               />
@@ -46,28 +66,8 @@ function Projects() {
             <Col md={4} className="project-card">
               <ProjectCard
                   isBlog={false}
-                  title="Under Construction"
-                  description=""
-                  ghLink="https://github.com/tomsbados"
-
-              />
-            </Col>
-
-            <Col md={4} className="project-card">
-              <ProjectCard
-                  isBlog={false}
-                  title="Under Construction"
-                  description=""
-                  ghLink="https://github.com/tomsbados"
-
-              />
-            </Col>
-
-            <Col md={4} className="project-card">
-              <ProjectCard
-                  isBlog={false}
-                  title="Under Construction"
-                  description=""
+                  title="Trash Identification Using YoloV8"
+                  description="As a project for my internship in Environmental Health and Safety, I implemented a YoloV8 computer vision model to identify mobile trash."
                   ghLink="https://github.com/tomsbados"
 
               />
